58655a53a92852dbdf9e06b6c0f2c2195e3c2f76
Tim Irmler <zwazel@hotmail.com>
2025-01-15 09:57:51 +0100

    wip

---
+++ render.py
@@ -0,0 +1,6 @@
+﻿import pygame
+
+# Initialize the game engine. full screen. have a circle in the middle (table)
+# make it a class, that can be constructed with a list of bots
+# each bot has a name. every bot is placed around the table. first bot at the top
+# the bots name is displayed there
